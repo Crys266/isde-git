4196ffa7cff0051d18b55bf79680b8d830feccf8
albepe01 <01albe01@gmail.com>
2023-10-20 13:16:33 +0200

    Implementazione split_data

--- src/fun_utils.py
+++ src/fun_utils.py
@@ -33,10 +33,8 @@ def split_data(x, y, tr_fraction=0.5):
     idx = list(range(0, n_samples))  # [0 1 ... 999]  np.linspace
     np.random.shuffle(idx)
     n_tr = int(tr_fraction * n_samples)
-
     idx_tr = idx[:n_tr]
     idx_ts = idx[n_tr:]
-
     xtr = x[idx_tr, :]
     ytr = y[idx_tr]
     xts = x[idx_ts, :]
